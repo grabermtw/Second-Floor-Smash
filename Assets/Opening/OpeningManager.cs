df8983198b2263dfddefaec6c18836050b5ec179
grabermtw <grabermtw@gmail.com>
2020-04-17 18:15:00 -0400

    Added new opening animation using CineMachine

--- Assets/Opening/OpeningManager.cs
+++ Assets/Opening/OpeningManager.cs
@@ -5,28 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class OpeningManager : MonoBehaviour
 {
-
+    public bool UseRandomCharacters;
+    public string placeholderCharacterTag;
     public CharacterList characterList;
     public Transform charPosParent;
     private Transform[] charPositions;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        // Remember that charPositions[0] will be the parent's transform, which we'll want to ignore
-        charPositions = charPosParent.GetComponentsInChildren<Transform>(false);
-
-        List<GameObject[]> charList = characterList.GetAllCharactersList();
-
-        // Start at i=1 to skip the parent transform
-        for (int i = 1; i < charPositions.Length; i++)
+        if (UseRandomCharacters)
         {
-            int charIndex = (int)Random.Range(0, charList.Count);
-            GameObject[] currChar = charList[charIndex];
-            charList.RemoveAt(charIndex);
+            // Deactivate any placeholder characters that are currently in the scene
+            Rigidbody[] placeholders = FindObjectsOfType<Rigidbody>();
+            foreach (Rigidbody rb in placeholders)
+            {
+                if (rb.gameObject.CompareTag(placeholderCharacterTag))
+                {
+                    rb.gameObject.SetActive(false);
+                }
+            }
 
-            Instantiate(currChar[(int)Random.Range(0, currChar.Length)], charPositions[i]);
+
+            // Remember that charPositions[0] will be the parent's transform, which we'll want to ignore
+            charPositions = charPosParent.GetComponentsInChildren<Transform>(false);
+
+            List<GameObject[]> charList = characterList.GetAllCharactersList();
+
+            System.Random rnd = new System.Random();
+
+            // Start at i=1 to skip the parent transform
+            for (int i = 1; i < charPositions.Length; i++)
+            {
+                int charIndex = rnd.Next(0, charList.Count);
+                print("charIndex: " + charIndex);
+                print("i = " + i);
+                print("charList.Count: " + charList.Count);
+
+                GameObject[] currChar = charList[charIndex];
+                charList.RemoveAt(charIndex);
+
+                Instantiate(currChar[(int)Random.Range(0, currChar.Length)], charPositions[i]);
+
+            }
         }
     }
 
