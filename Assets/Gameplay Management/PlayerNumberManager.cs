4db894a3b7f915dffb3118788590ab32b98caf4e
grabermtw <grabermtw@gmail.com>
2020-04-17 23:46:36 -0400

    Began work on a dynamic camera for the gameplay using Cinemachine

--- Assets/Gameplay Management/PlayerNumberManager.cs
+++ Assets/Gameplay Management/PlayerNumberManager.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class PlayerNumberManager : MonoBehaviour
 {
     private int playerNumber = 1;
     public GameObject[] playerTextObjects;
     public GameObject[] playerDamageTexts;
-    PlayerCharList playerCharList;
-    List<GameObject> playerChoices;
-    List<InputDevice> playerDevices;
+    public CinemachineTargetGroup targetGroup;
+    private PlayerCharList playerCharList;
+    private List<GameObject> playerChoices;
+    private List<InputDevice> playerDevices;
 
     // Start is called before the first frame update
     void Start()
@@ -29,10 +31,32 @@ public class PlayerNumberManager : MonoBehaviour
 
             Debug.Log("Instantiating " + choice.tag);
             Debug.Log("Device Number " + deviceNum + " out of " + playerDevices.Count);
-            PlayerInput.Instantiate(choice, -1, null, -1, playerDevices[deviceNum]);
+            PlayerInput newPlayer = PlayerInput.Instantiate(choice, -1, null, -1, playerDevices[deviceNum]);
             Debug.Log("Instantiated " + choice.tag);
             deviceNum++;
 
+            // Add each character as a target for the camera
+            CinemachineTargetGroup.Target target;
+            target.target = null;
+            // Find the character's hips, because otherwise the camera will focus on their feet
+            foreach (Transform tr in newPlayer.gameObject.transform.GetComponentsInChildren<Transform>())
+            {
+                if (tr.gameObject.name == "mixamorig:Hips")
+                {
+                    target.target = tr;
+                    break;
+                }
+            }
+            target.weight = 1;
+            target.radius = 1;
+            for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+            {
+                if (targetGroup.m_Targets[i].target == null)
+                {
+                    targetGroup.m_Targets.SetValue(target, i);
+                    break;
+                }
+            }
         }
 
     }
