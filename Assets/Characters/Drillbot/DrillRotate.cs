1fa24677913196d8debd79b93496ad5eca27a4c9
Matthew Vorsteg <mvorsteg7@gmail.com>
2023-06-24 21:11:41 -0400

    More fixes and animations for new character

---
+++ Assets/Characters/Drillbot/DrillRotate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrillRotate : MonoBehaviour
+{
+    [SerializeField] private float rotation_DegPerSec;
+    [SerializeField] private Transform drillObj;
+
+    private void Update()
+    {
+        if (drillObj != null)
+        {
+            drillObj.Rotate(Vector3.up * rotation_DegPerSec * Time.deltaTime, Space.Self);
+        }
+    }
+}
